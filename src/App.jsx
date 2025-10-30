9a50b1dcbac69c34b18cc3f69c82f429f6e4e446
VKarada <221110851@students.mikroskil.ac.id>
2025-10-31 00:09:58 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Simple ToDo List</h1>
+      <h1>ToDo List</h1>
 
       {/* Form untuk menambah task baru */}
       <form onSubmit={handleAddTask}>
